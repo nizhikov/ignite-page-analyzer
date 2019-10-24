d1b1b653d1db883e5f54da42d6ee083cda7b21f4
shishkovilja <shishkovilja@yandex.ru>
2019-10-24 17:40:21 +0300

    add-first-tests: Initial commit

---
+++ src/test/scala/ru/nizhikov/ignite/pagestat/IgnitePageFreeSpaceCalculationTest.scala
@@ -0,0 +1,21 @@
+package ru.nizhikov.ignite.pagestat
+
+import org.scalatest.{BeforeAndAfterEach, FlatSpec}
+
+class IgnitePageFreeSpaceCalculationTest extends FlatSpec with BeforeAndAfterEach {
+
+    override def beforeEach() {
+
+    }
+
+    override def afterEach() {
+
+    }
+
+    behavior of "IgnitePageFreeSpaceCalculationTest"
+
+    it should "IgnitePage" in {
+
+    }
+
+}
