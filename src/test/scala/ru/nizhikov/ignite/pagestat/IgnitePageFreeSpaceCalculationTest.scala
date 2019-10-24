cac3ab27f09cad7b3f2548c6d6a75bad6524fe5d
shishkovilja <shishkovilja@yandex.ru>
2019-10-24 18:13:09 +0300

    add-first-tests: Add support for page types T_DATA (1) and T_PAGE_LIST_NODE (13)

--- src/test/scala/ru/nizhikov/ignite/pagestat/IgnitePageFreeSpaceCalculationTest.scala
+++ src/test/scala/ru/nizhikov/ignite/pagestat/IgnitePageFreeSpaceCalculationTest.scala
@@ -1,21 +1,60 @@
 package ru.nizhikov.ignite.pagestat
 
+import java.nio.{ByteBuffer, ByteOrder}
+
+import org.apache.ignite.configuration.DataStorageConfiguration.DFLT_PAGE_SIZE
+import org.apache.ignite.internal.processors.cache.persistence.tree.io.PageIO.{COMMON_HEADER_END, T_DATA, T_PAGE_LIST_NODE}
 import org.scalatest.{BeforeAndAfterEach, FlatSpec}
 
+/**
+ * Simple test providing checks of [[IgnitePage()]]#getFreeSpace calculation for supported by page-analyzer page types
+ */
 class IgnitePageFreeSpaceCalculationTest extends FlatSpec with BeforeAndAfterEach {
+    private val TEST_PAGE_SIZE = DFLT_PAGE_SIZE
 
-    override def beforeEach() {
+    // Test values for pages
+    private val DATA_PAGE_TEST_FREE_SPACE: Int = 42
+    private val PAGES_LIST_NODE_TEST_COUNT: Int = 77
+
+    // Fake buffer, acts as mock page buffer
+    var fakePageBuffer: ByteBuffer = ByteBuffer.allocate(TEST_PAGE_SIZE)
+        .order(ByteOrder.nativeOrder())
+
+    override def beforeEach(): Unit = {
 
     }
 
     override def afterEach() {
-
+        fakePageBuffer.clear()
     }
 
-    behavior of "IgnitePageFreeSpaceCalculationTest"
+    behavior of "Pages of type T_DATA=1 (AbstractDataPageIO)"
 
-    it should "IgnitePage" in {
+    it should "provide correct free space value" in {
+        fakePageBuffer.putShort(COMMON_HEADER_END + 8, DATA_PAGE_TEST_FREE_SPACE.toShort)
 
+        val freeSpace = fakePageBuffer.pageFreeSpace(T_DATA)
+            .getOrElse(0)
+        assert(freeSpace === DATA_PAGE_TEST_FREE_SPACE)
     }
 
+    behavior of "Pages of type T_PAGE_LIST_NODE=13 (PagesListNodeIO)"
+
+    it should "provide correct free space value" in {
+        fakePageBuffer.putShort(COMMON_HEADER_END + 8 + 8, PAGES_LIST_NODE_TEST_COUNT.toShort)
+
+        val freeSpace = fakePageBuffer.pageFreeSpace(T_PAGE_LIST_NODE)
+            .getOrElse(0)
+        assert(freeSpace === getTestPageListNodeFreeSpace)
+    }
+
+
+    // Calculate free space test value for PagesListNodeIO
+    private def getTestPageListNodeFreeSpace: Int = {
+        val PAGE_IDS_OFF = COMMON_HEADER_END + 8 + 8 + 2
+
+        val cnt = PAGES_LIST_NODE_TEST_COUNT
+        val capacity = (TEST_PAGE_SIZE - PAGE_IDS_OFF) >>> 3
+        (capacity - cnt) * 8
+    }
 }
